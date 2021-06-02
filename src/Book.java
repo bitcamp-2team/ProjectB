24876367b400909d8d44b55dc7d987bd6082def0
up456 <up456@users.noreply.github.com>
2021-06-02 20:12:00 +0900

    book인터페이스 복구

--- src/Book.java
+++ src/Book.java
@@ -1,10 +1,27 @@
  public interface Book {
 	
-	void searchMenu();			// 검색기능 
-	void searchForName();		// 도서제목
-	void searchForNum();		// 도서번호
-	void searchForAuthor();		// 도서저자
-	void searchForPublisher();	// 도서출판사
+	 /** 메뉴출력*/ // 최상->메뉴 출력 및 전체 구조 효미
 
-	void showBookInfo();		// 도서전체목록 조회
+		/** 1.검색기능 4종 [1.도서제목, 2.책번호, 3.출판사, 4.저자] */
+		void searchMenu(); //상    //승옥
+		 
+		void searchForName();
+		void searchForNum();
+		void searchForAuthor();
+		void searchForPublisher();
+
+		/**  2.책 정보 조회*/  //하  //승옥
+		void showBookInfo();
+
+		/**  3. 도서 등록   [도서제목, 책번호, 출판사, 저자, 재고] */  //중  //하윤
+		void addBook();
+
+		/**  4. 도서 삭제*/   //중  //하윤
+		void deleteBook();
+
+		/**  5. 도서 대여*/   //상  // 세은 
+		void rendBook();
+
+		/**  6. 도서 반납*/  //상  // 세은 
+		void backBook();
 }
