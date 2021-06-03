fac5c449a1fe850f845edc17b18509efc9aa0cf7
세은 <@DESKTOP-FAKPTRO>
2021-06-03 16:23:06 +0900

    추가

--- src/BookVo.java
+++ src/BookVo.java
@@ -1,23 +1,23 @@
 
 public class BookVo {
 	private String bTitle;
-	private String bNum;
+	private static int bNum;
 	private String bAuthor;
 	private String bPublisher;
 	private int bStock;
 	private boolean Loaned;  
-	public boolean equals(String id) {
-		  if (this.bNum.equals(id))
-		   return true;
-		  else
-		   return false;
+	boolean equals(String id) {
+		  if (this.bNum==Integer.parseInt(id)) {
+		   return true;}
+		  else {
+		   return false;}
 		 }
 
 	//湲곌컙 異뷀썑 異붽� �삊�쓽
 	
 	
 	/** �깮�꽦�옄 �븣留욊쾶 �닔�젙�븯�꽭�슂*/
-	public BookVo(String bTitle, String bNum, String bAuthor, String bPublisher, int bStock,boolean Loaned) {
+	public BookVo(String bTitle, int bNum, String bAuthor, String bPublisher, int bStock,boolean Loaned) {
 		super();
 		this.bTitle = bTitle;
 		this.bNum = bNum;
@@ -25,6 +25,7 @@ public class BookVo {
 		this.bPublisher = bPublisher;
 		this.bStock = bStock;
 		this.Loaned=Loaned;
+		
 	}
 	public BookVo() {};  //湲곕낯�깮�꽦�옄
 	
@@ -36,10 +37,10 @@ public class BookVo {
 	public void setbTitle(String bTitle) {
 		this.bTitle = bTitle;
 	}
-	public String getbNum() {
+	public int getbNum() {
 		return bNum;
 	}
-	public void setbNum(String bNum) {
+	public void setbNum(int bNum) {
 		this.bNum = bNum;
 	}
 	public String getbAuthor() {
